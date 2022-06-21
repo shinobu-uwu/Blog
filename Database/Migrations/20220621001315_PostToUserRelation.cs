1f9dbb39aa5f17e24c3e6edaeb890b873830d885
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-06-20 21:22:58 -0300

    Create relation between post and user

---
+++ Database/Migrations/20220621001315_PostToUserRelation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Database.Migrations
+{
+    public partial class PostToUserRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_AuthorId",
+                table: "Posts",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Posts");
+        }
+    }
+}
