22528622b18df903fdfd181340018bd1e9fc4102
Matheus Filip dos Santos Reinert <matheus.reinert@protonmail.com>
2022-06-26 19:29:11 -0300

    Create EditPostViewModel

---
+++ ViewModels/EditPostViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels;
+
+public class EditPostViewModel
+{
+    public int PostId { get; set; }
+
+    [Required(ErrorMessage = "You must give your post a title")]
+    [MaxLength(20, ErrorMessage = "Title must be up to 20 characters long")]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters long")]
+    public string Title { get; set; }
+
+    public string Body { get; set; }
+}
